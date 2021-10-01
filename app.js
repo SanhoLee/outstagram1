3048e9ccefa08e5345290f0467044d5cc6dedea8
SanhoLee <stlsh0516@gmail.com>
2021-10-01 23:12:22 +0900

    make express server simply by docs.

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+app.get("/", (req, res) => {
+  res.send("hello world ! I'm Lee. ");
+});
+
+app.listen(4000, () => {
+  console.log(`App Listening ... : http://localhost:${PORT}`);
+});
