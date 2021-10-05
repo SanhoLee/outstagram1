e4692b9622c6f4f3229252c307559e8bb940a8d6
SanhoLee <stlsh0516@gmail.com>
2021-10-05 20:33:29 +0900

    middleware update.

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 import express from "express";
-import routes from "./routes";
+import cookieParser from "cookie-parser";
 import globalRouter from "./routers/globalRouter";
 import meRouter from "./routers/meRouter";
 import searchRouter from "./routers/searchRouter";
+import routes from "./routes";
+import helmet from "helmet";
+import morgan from "morgan";
 
 const app = express();
 
-app.get(routes.home, globalRouter);
-app.get(routes.me, meRouter);
-app.get(routes.search, searchRouter);
+// middleware Setting
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.json()); //original was body-parser.
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev")); //logger infomation.
+
+// Router Setting
+app.use(routes.home, globalRouter);
+app.use(routes.me, meRouter);
+app.use(routes.search, searchRouter);
 
 export default app;
