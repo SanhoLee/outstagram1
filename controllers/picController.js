9dba524d2f8801ce86aa2b1f86c64cb2834e49dc
SanhoLee <stlsh0516@gmail.com>
2021-10-15 22:35:02 +0900

    pic array on page home with fake db.

--- controllers/picController.js
+++ controllers/picController.js
@@ -1,1 +1,78 @@
+let pics = [
+  {
+    id: 1,
+    username: "fwanggus",
+    userAvatar: "/user_img.jpg",
+    picUrl: "/IMG_2675.jpg",
+    numLiked: 10,
+    commentIds: [1, 4, 7, 10],
+  },
+  {
+    id: 2,
+    username: "gyul",
+    userAvatar: "/user_img.jpg",
+    picUrl: "/IMG_3736.JPG",
+    numLiked: 3,
+    commentIds: [10, 40, 70, 100],
+  },
+  {
+    id: 3,
+    username: "sanho",
+    userAvatar: "/user_img.jpg",
+    picUrl: "/jeju_bg.jpeg",
+    numLiked: 2,
+    commentIds: [3, 6, 9, 19],
+  },
+  {
+    id: 3,
+    username: "sanho",
+    userAvatar: "/user_img.jpg",
+    picUrl: "/jeju_bg.jpeg",
+    numLiked: 2,
+    commentIds: [3, 6, 9, 19],
+  },
+  {
+    id: 3,
+    username: "sanho",
+    userAvatar: "/user_img.jpg",
+    picUrl: "/jeju_bg.jpeg",
+    numLiked: 2,
+    commentIds: [3, 6, 9, 19],
+  },
+  {
+    id: 2,
+    username: "gyul",
+    userAvatar: "/user_img.jpg",
+    picUrl: "/IMG_3736.JPG",
+    numLiked: 3,
+    commentIds: [10, 40, 70, 100],
+  },
+  {
+    id: 2,
+    username: "gyul",
+    userAvatar: "/user_img.jpg",
+    picUrl: "/IMG_3736.JPG",
+    numLiked: 3,
+    commentIds: [10, 40, 70, 100],
+  },
+  {
+    id: 2,
+    username: "gyul",
+    userAvatar: "/user_img.jpg",
+    picUrl: "/IMG_3736.JPG",
+    numLiked: 3,
+    commentIds: [10, 40, 70, 100],
+  },
+  {
+    id: 2,
+    username: "gyul",
+    userAvatar: "/user_img.jpg",
+    picUrl: "/IMG_3736.JPG",
+    numLiked: 3,
+    commentIds: [10, 40, 70, 100],
+  },
+];
 
+export const home = async (req, res) => {
+  await res.render("home", { pics });
+};
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
+import { home } from "../controllers/picController";
 import { getMe } from "../controllers/userController";
 import routes from "../routes";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.home, (req, res) => res.render("home"));
+globalRouter.get(routes.home, home);
 globalRouter.get(routes.login, (req, res) => res.render("login"));
 globalRouter.get(routes.join, (req, res) => res.render("join"));
 globalRouter.get(routes.me, getMe);
