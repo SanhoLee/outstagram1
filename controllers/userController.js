8af3912dac998a8bbb8f90b02b6acbc95eb6ae70
SanhoLee <stlsh0516@gmail.com>
2021-10-26 21:51:59 +0900

    post method checking for join page.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,17 @@
+import routes from "../routes";
+
 export const getMe = (req, res) => {
   res.render("me");
+};
+
+export const getJoin = (req, res) => {
+  res.render("join");
+};
+
+export const postJoin = (req, res) => {
+  const {
+    body: { email, name, username, password },
+  } = req;
+
+  res.redirect(routes.home);
 };
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,13 +1,16 @@
 import express from "express";
 import { home } from "../controllers/picController";
-import { getMe } from "../controllers/userController";
+import { getJoin, getMe, postJoin } from "../controllers/userController";
 import routes from "../routes";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.login, (req, res) => res.render("login"));
-globalRouter.get(routes.join, (req, res) => res.render("join"));
+
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
 globalRouter.get(routes.me, getMe);
 
 // need to decide how to handle search flow.
